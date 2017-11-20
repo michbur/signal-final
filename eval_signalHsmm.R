bc8438ed5d7b71cb21361b1ca91d2cb383380d95
michbur <michalburdukiewicz@gmail.com>
2017-11-20 10:49:02 +0100

    first commit

---
+++ eval_signalHsmm.R
@@ -0,0 +1,17 @@
+library(seqinr)
+library(signalHsmm)
+library(dplyr)
+library(pbapply)
+
+files_path <- "/home/michal/Dropbox/signal_final_dat/"
+
+res <- pblapply(list.files(paste0(files_path, "data/"))[1L:3], function(ith_file) {
+  dat <- read.fasta(paste0(files_path, "data/", ith_file), seqtype = "AA")
+  run_signalHsmm(dat[1L:100]) %>% 
+    pred2df %>% 
+    mutate(file_name = ith_file, seq_name = rownames(.)) %>% 
+    select(file_name, seq_name, sp.probability)
+}) %>% 
+  do.call(rbind, .)
+
+write.csv(x = res, file = paste0(files_path, "results/res.csv"), row.names = FALSE)
