212b1bb8585bfbac37be317fae6ea90b101bf836
michbur <michalburdukiewicz@gmail.com>
2017-11-20 20:43:48 +0100

    reverted full analysis

--- eval_signalHsmm.R
+++ eval_signalHsmm.R
@@ -6,12 +6,23 @@ library(pbapply)
 files_path <- "/home/michal/Dropbox/signal_final_dat/"
 
 res <- pblapply(list.files(paste0(files_path, "data/")), function(ith_file) {
+
   dat <- read.fasta(paste0(files_path, "data/", ith_file), seqtype = "AA")
-  try({run_signalHsmm(dat) %>% 
-    pred2df %>% 
-    mutate(file_name = ith_file, seq_name = rownames(.)) %>% 
-    select(file_name, seq_name, sp.probability)
-  }, silent = TRUE)
+  res <- unlist(lapply(dat, function(ith_seq) try(run_signalHsmm(ith_seq), silent = TRUE)), recursive = FALSE)
+  
+  lapply(res, function(ith_res) {
+    if(class(ith_res) == "try_error") {
+      sp.probability = NA
+    } else {
+      sp.probability = ith_res[["sp_probability"]]
+    }
+  }) %>% 
+    unlist(use.names = FALSE) %>% 
+    data.frame(prob = .) %>% 
+    mutate(name = names(res),
+           id = 1L:nrow(.),
+           file = ith_file) %>% 
+    select(file, id, name, prob)
 })
 
-save(res, file = paste0(files_path, "results/res.RData"))
+write.csv(x = res, file = paste0(files_path, "results/res.csv"), row.names = FALSE)
