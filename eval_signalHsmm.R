bd78d8643c528b5e14424a5968c1ab8551be7e31
michbur <michalburdukiewicz@gmail.com>
2018-02-06 22:55:13 +0100

    hopefully did properly eval

--- eval_signalHsmm.R
+++ eval_signalHsmm.R
@@ -5,24 +5,45 @@ library(pbapply)
 
 files_path <- "/home/michal/Dropbox/signal_final_dat/"
 
-res <- pblapply(list.files(paste0(files_path, "data/")), function(ith_file) {
-
-  dat <- read.fasta(paste0(files_path, "data/", ith_file), seqtype = "AA")
-  res <- unlist(lapply(dat, function(ith_seq) try(run_signalHsmm(ith_seq), silent = TRUE)), recursive = FALSE)
+all_seqs_res <- pblapply(list.files(paste0(files_path, "data/")), function(ith_file) {
   
-  lapply(res, function(ith_res) {
-    sp.probability  <- if("sp_probability" %in% names(ith_res)) {
-      ith_res[["sp_probability"]]
-    } else {
-      NA
-    }
-    data.frame(name = ith_res[["name"]], signalHsmm = sp.probability)
-  }) %>% 
-    do.call(rbind, .) %>% 
-    mutate(id = 1L:nrow(.),
-           file = ith_file) %>% 
-    select(file, id, name, signalHsmm)
+  dat <- read.fasta(paste0(files_path, "data/", ith_file), seqtype = "AA")
+  res <- lapply(dat, function(ith_seq) try({
+    signalHsmm_pred <- run_signalHsmm(ith_seq)
+    
+    write.fasta(ith_seq, name = attr(ith_seq, "name"), file.out = "tmp_seq.fasta")
+    
+    signalP_command <- paste0(files_path, "signalp-4.1/signalp -t euk -f short tmp_seq.fasta > signalP.txt")
+    deepSig_command <- paste0(files_path, "deepsig-1.0/runDeepSig.sh tmp_seq.fasta euk deepSig.txt")
+
+    system(deepSig_command, ignore.stdout = TRUE, ignore.stderr = TRUE)
+    system(signalP_command)
+    
+    res_deepSig <- read.table("deepSig.txt") %>% 
+      mutate(deepSig = ifelse(V2 == "SignalPeptide", V3, 1 - V3)) %>% 
+      select(deepSig) %>% 
+      unlist
+    
+    res_signalP <- read.table("signalP.txt", skip = 1) %>% 
+      mutate(signalP = V9) %>% 
+      select(signalP) %>% 
+      unlist
+    
+    file.remove(c("tmp_seq.fasta", "deepSig.txt", "signalP.txt"))
+    
+    data.frame(file = ith_file, 
+               name = attr(ith_seq, "name"),
+               signalHsmm = signalHsmm_pred[[1]][["sp_probability"]],
+               deepSig = res_deepSig,
+               signalP = res_signalP,
+               stringsAsFactors = FALSE)
+  }, silent = TRUE)) 
+  
+  nonproblematic <- sapply(res, function(i) class(i) != "try-error")
+  do.call(rbind, res[nonproblematic]) %>% 
+    mutate(id = cumsum(nonproblematic)) %>% 
+    select(file, id, name, signalHsmm, signalP, deepSig)
 }) %>% 
   do.call(rbind, .)
 
-write.csv(x = res, file = paste0(files_path, "results/res.csv"), row.names = FALSE)
+write.csv(x = all_seqs_res, file = paste0(files_path, "results/full_res.csv"), row.names = FALSE)
--- other_preds.R
+++ other_preds.R
@@ -5,16 +5,35 @@ files_path <- "/home/michal/Dropbox/signal_final_dat/"
 
 res <- pblapply(list.files(paste0(files_path, "data/")), function(ith_file) {
   
-  deepSig_command <- paste0(files_path, "deepsig-1.0/runDeepSig.sh ", files_path, ith_file, " euk tmp.txt")
-  system(deepSig_command)
-  
-  res <- read.table("tmp.txt") %>% 
+  browser()
+  signalP_command <- paste0(files_path, "signalp-4.1/signalp -t euk -f short ", 
+                            files_path, "data/", ith_file, " > signalP.txt")
+  deepSig_command <- paste0(files_path, "deepsig-1.0/runDeepSig.sh ", 
+                            files_path, "data/", ith_file, " euk deepSig.txt")
+  system(deepSig_command, ignore.stdout = TRUE)
+  system(signalP_command, ignore.stdout = TRUE)
+  #V1            V2 V3 V4
+  #1 XP_001613005.1; SignalPeptide  1 25
+  #2 XP_001613005.1; SignalPeptide  1 23
+  res_deepSig <- read.table("deepSig.txt") %>% 
     mutate(deepSig = ifelse(V2 == "SignalPeptide", V3, NA),
+           file = ith_file) %>% 
+    filter(!duplicated(.)) %>% 
+    select(file, name = V1, deepSig)
+
+  res_signalP <- read.table("signalP.txt", skip = 1) %>% 
+    mutate(signalP = V9,
            id = 1L:nrow(.),
            file = ith_file) %>% 
-    select(file, id, name = V1, deepSig)
-
-  file.remove("tmp.txt")
+    select(file, id, name = V1, signalP)
+  
+  setdiff(as.character(res_deepSig[["name"]]),
+          as.character(res_signalP[["name"]]))
+  filter(res_deepSig, name == "XP_001612976.1;")
+  
+  read.table("deepSig.txt")
+  
+  file.remove("deepSig.txt")
   res
 }) %>% 
   do.call(rbind, .)
